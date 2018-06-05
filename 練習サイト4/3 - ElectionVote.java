120a1226a9071a955371c8344e223c2280f702fc
tomokiokumura <tomoki_okumura@unirita.co.jp>
2018-06-05 16:35:03 +0900

    [fixing] now fixing this java file...
    当選確実になるまでの判定が正しくなかったので修正中

--- 練習サイト4/3 - ElectionVote.java
+++ 練習サイト4/3 - ElectionVote.java
@@ -2,48 +2,50 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
+import java.util.Map.Entry;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         String[] C = br.readLine().split(" ");
-        HashMap<String, Integer> votedMap = new HashMap();
+        HashMap<String, Integer> votedMap = new HashMap<>();
         int max = 0;
         String name = new String();
         int vote = 0;
-        for (int i = 0; i < C.length; i++) {
+        boolean flag = false;
+
+        for (int i = 0; i < C.length; ++i) {
+            vote++;
             if (!votedMap.containsKey(C[i])) {
                 votedMap.put(C[i], 1);
+            } else {
+                votedMap.put(C[i], votedMap.get(C[i]) + 1);
             }
-            else {
-                int temp = votedMap.get(C[i]);
-                temp++;
-                votedMap.replace(C[i], temp);
-            }
+
             if (votedMap.get(C[i]) > max) {
                 max = votedMap.get(C[i]);
                 name = C[i];
-                vote = i + 1;
             }
-            if (votedMap.get(C[i]) > N / 2) {
-                name = C[i];
-                vote = i + 1;
+            if(){ // 当確になったら
+                ~flag;
+            }
+
+            if (flag) {
                 break;
             }
         }
 
         int counter = 0;
-        for (int i = 0; i < votedMap.size(); i++) {
-            if (votedMap.get(C[i]) == max) {
-                counter++;
+        for (Entry<String, Integer> map : votedMap.entrySet()) {
+            if (map.getValue() == max) {
+                ++counter;
             }
         }
 
         if (counter > 1) {
             System.out.println("TIE");
-        }
-        else {
+        } else {
             System.out.println(name + " " + vote);
         }
     }
