9b0c59fd76cd860f1a04ea9d945577170770da50
Unknown <tomoki_okumura@unirita.co.jp>
2018-06-20 10:01:30 +0900

    [update] update java file
    完成

--- 練習サイト4/3 - ElectionVote.java
+++ 練習サイト4/3 - ElectionVote.java
@@ -1,52 +1,40 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map.Entry;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         String[] C = br.readLine().split(" ");
-        HashMap<String, Integer> votedMap = new HashMap<>();
-        int max = 0;
-        String name = new String();
-        int vote = 0;
-        boolean flag = false;
 
-        for (int i = 0; i < C.length; ++i) {
-            vote++;
-            if (!votedMap.containsKey(C[i])) {
-                votedMap.put(C[i], 1);
-            } else {
-                votedMap.put(C[i], votedMap.get(C[i]) + 1);
-            }
+        int[] charCount = new int[26];
+        int voteCount = 0;
+        int first = 0;
+        int second = 0;
 
-            if (votedMap.get(C[i]) > max) {
-                max = votedMap.get(C[i]);
-                name = C[i];
-            }
-            if(){ // 当確になったら
-                ~flag;
-            }
+        for (String s : C) {
+            charCount[s.charAt(0) - 'A']++;
+            voteCount++;
 
-            if (flag) {
+            int[] temp = charCount.clone();
+            Arrays.sort(temp);
+            first = temp[temp.length - 1];
+            second = temp[temp.length - 2];
+            if (N - voteCount < first - second) {
                 break;
             }
         }
 
-        int counter = 0;
-        for (Entry<String, Integer> map : votedMap.entrySet()) {
-            if (map.getValue() == max) {
-                ++counter;
-            }
-        }
-
-        if (counter > 1) {
+        if (first == second) {
             System.out.println("TIE");
         } else {
-            System.out.println(name + " " + vote);
+            for (int i = 0; i < charCount.length; i++) {
+                if (charCount[i] == first) {
+                    System.out.println((char)('A' + i) + " " + voteCount);
+                }
+            }
         }
     }
 }
